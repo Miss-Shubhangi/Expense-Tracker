6a9e0cb2d626e33fc4a16e5974cfa2c3aee55e8e
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-07-27 17:18:21 +0530

    schema is created for user and for transactions

---
+++ server/models/Transaction.js
@@ -0,0 +1,23 @@
+import { Schema ,model } from "mongoose";
+import { User } from "./User.js";
+
+const transactionSchema = new Schema ({
+    amount :{
+        type: Number,
+        required:true
+    },
+    category:{
+        type: String,
+        default:"others"
+    },
+    type:{
+        type:String,
+        enum:["Debit" ,"Credit"]
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+const Transaction = model("Transaction" , transactionSchema)
